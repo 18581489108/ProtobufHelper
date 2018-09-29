e0aa85c7be4a4cb52b249297d4386ab1c2ec5ece
18581489108 <18581489108@163.com>
2018-09-29 21:41:22 +0800

    初始化项目

---
+++ src/main/java/cn/kurisu9/App.java
@@ -0,0 +1,14 @@
+package cn.kurisu9;
+
+import com.google.protobuf.DescriptorProtos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println(DescriptorProtos.FileDescriptorProto.class);
+    }
+}
