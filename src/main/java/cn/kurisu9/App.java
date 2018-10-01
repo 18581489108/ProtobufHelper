554496df3e0bceab933cad1b2cd4bfa4a71e3dfe
18581489108 <18581489108@163.com>
2018-10-01 16:25:37 +0800

    在配置中增加protoc文件的路径

--- src/main/java/cn/kurisu9/App.java
+++ src/main/java/cn/kurisu9/App.java
@@ -1,13 +1,86 @@
 package cn.kurisu9;
 
-import com.google.protobuf.DescriptorProtos;
+import cn.kurisu9.config.Config;
+import cn.kurisu9.utils.process.Command;
+import cn.kurisu9.utils.process.ExecResult;
+import cn.kurisu9.utils.process.ProcessUtils;
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static cn.kurisu9.GlobalSetting.*;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println(DescriptorProtos.FileDescriptorProto.class);
+
+    /**
+     * 启动参数
+     *
+     * args[0]: config的路径
+     *
+     * */
+    public static void main(String[] args) throws Exception {
+        String configPath;
+        if (args != null && args.length > 0) {
+            configPath = args[0];
+        } else {
+            configPath  = CONFIG_PATH;
+        }
+
+        File configFile = new File(configPath);
+        if (!configFile.exists()) {
+            throw new FileNotFoundException("Don't find config.json in " + configPath);
+        }
+
+        String content = FileUtils.readFileToString(configFile, FILE_ENCODING);
+        Config config = JSON.parseObject(content, Config.class);
+
+        System.out.println(config.getProtocFile());
+        spawnDescFile(config);
+    }
+
+    /**
+     * 生成描述文件
+     * */
+    private static void spawnDescFile(Config config) {
+        String cmd = ".\\tools\\protoc3.5.1.exe -I=C:\\Users\\zhaoxin_m\\Desktop\\protobuf\\test\\ --descriptor_set_out=./build/desc/example.desc Example.proto";
+
+        Command command = new Command(".\\tools\\protoc3.5.1.exe");
+        command.addParam("-I=C:\\Users\\zhaoxin_m\\Desktop\\protobuf\\test\\");
+        command.addParam("--descriptor_set_out=./build/desc/example.desc");
+        command.addParam("Example.proto");
+
+        ExecResult result = ProcessUtils.exec(command);
+
+        System.out.println(result.isSuccess());
+        System.out.println(result.getOut());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/cn/kurisu9/config/Config.java
+++ src/main/java/cn/kurisu9/config/Config.java
@@ -7,6 +7,11 @@ package cn.kurisu9.config;
  **/
 public class Config {
     /**
+     * 编译proto文件的工具路径
+     * */
+    private String protocFile;
+
+    /**
      * 输出的临时目录
      * */
     private String tempRootPath;
@@ -25,4 +30,44 @@ public class Config {
      * 输出配置
      * */
     private OutConfig[] outConfigs;
+
+    public String getProtocFile() {
+        return protocFile;
+    }
+
+    public void setProtocFile(String protocFile) {
+        this.protocFile = protocFile;
+    }
+
+    public String getTempRootPath() {
+        return tempRootPath;
+    }
+
+    public void setTempRootPath(String tempRootPath) {
+        this.tempRootPath = tempRootPath;
+    }
+
+    public String getDescOutPath() {
+        return descOutPath;
+    }
+
+    public void setDescOutPath(String descOutPath) {
+        this.descOutPath = descOutPath;
+    }
+
+    public ProtoConfig getProtoConfig() {
+        return protoConfig;
+    }
+
+    public void setProtoConfig(ProtoConfig protoConfig) {
+        this.protoConfig = protoConfig;
+    }
+
+    public OutConfig[] getOutConfigs() {
+        return outConfigs;
+    }
+
+    public void setOutConfigs(OutConfig[] outConfigs) {
+        this.outConfigs = outConfigs;
+    }
 }
