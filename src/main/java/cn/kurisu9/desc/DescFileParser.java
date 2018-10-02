9904c0e0f72c49ef50ae11a6017d5c1786f09b12
18581489108 <18581489108@163.com>
2018-10-02 20:32:34 +0800

    添加描述文件解析类

---
+++ src/main/java/cn/kurisu9/desc/DescFileParser.java
@@ -0,0 +1,123 @@
+package cn.kurisu9.desc;
+
+import cn.kurisu9.GlobalContext;
+import cn.kurisu9.data.Result;
+import cn.kurisu9.utils.process.Command;
+import cn.kurisu9.utils.process.ExecResult;
+import cn.kurisu9.utils.process.ProcessUtils;
+import com.google.protobuf.DescriptorProtos;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+import static cn.kurisu9.GlobalSetting.DESC_EXTENSION;
+
+/**
+ * @author zhaoxin_m
+ * @description 描述文件解析
+ * @date 2018/10/2 18:56
+ **/
+public class DescFileParser {
+    private GlobalContext context;
+
+
+    public DescFileParser(GlobalContext context) {
+        this.context = context;
+    }
+
+    public Result parse() {
+        Result result = spawnDescFiles();
+        if (result.isFailed()) {
+            return result;
+        }
+
+        result = parseDecFiles();
+        if (result.isFailed()) {
+            return result;
+        }
+
+        return Result.DEFAULT_SUCCESS;
+    }
+
+    /**
+     * 生成描述文件
+     * */
+    private Result spawnDescFiles() {
+        String protocFile = context.getProtocFile().toAbsolutePath().toString();
+        String protoSrcPath = context.getProtoSrcPath().toAbsolutePath().toString();
+
+        Path descOutPath = context.getDescOutPath();
+        // 判断描述文件的父目录目录是否存在，不存在则创建
+        Path descParentPath = descOutPath.getParent();
+        if (!Files.exists(descParentPath)) {
+            try {
+                Files.createDirectories(descParentPath);
+            } catch (IOException e) {
+                return Result.forFailed(e.getMessage());
+            }
+        }
+
+        Command command = new Command(protocFile);
+        command.addParam("-I=" + protoSrcPath);
+        command.addParam("--descriptor_set_out=" + descOutPath.toAbsolutePath().toString());
+        String[] filesOfGenerateId = context.getConfig().getProtoConfig().getFilesOfGenerateId();
+        for (String file : filesOfGenerateId) {
+            command.addParam(file);
+        }
+
+        ExecResult result = ProcessUtils.exec(command);
+
+        if (!result.isSuccess()) {
+            return Result.forFailed(result.getOut());
+        }
+
+        return Result.DEFAULT_SUCCESS;
+    }
+
+    /**
+     * 解析描述文件
+     * */
+    private Result parseDecFiles() {
+        try {
+            DescriptorProtos.FileDescriptorSet fdSet = DescriptorProtos.FileDescriptorSet
+                    .parseFrom(Files.newInputStream(context.getDescOutPath().toAbsolutePath(), StandardOpenOption.READ));
+
+            // TODO 如何解析proto文件
+            for (DescriptorProtos.FileDescriptorProto fileDescriptorProto : fdSet.getFileList()) {
+                System.out.println(fileDescriptorProto.getName());
+            }
+        } catch (IOException e) {
+            return Result.forFailed(e.getMessage());
+        }
+
+
+        return Result.DEFAULT_SUCCESS;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
